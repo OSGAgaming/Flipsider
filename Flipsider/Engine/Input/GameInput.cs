055ff92e59b39e4a431ae590f6f3aad1bcb5dc36
BoffinWritesCode <reachjoshjohnson@gmail.com>
2020-09-17 22:48:46 +0100

    input system

---
+++ Flipsider/Engine/Input/GameInput.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Flipsider.Engine.Input
+{
+    public class GameInput
+    {
+        public static GameInput Instance;
+
+        static GameInput()
+        {
+            Instance = new GameInput();
+        }
+
+        private Dictionary<string, InputBinding> _controls;
+
+        public KeyboardState PreviousKeyState { get; private set; }
+        public KeyboardState CurrentKeyState { get; private set; }
+        public MouseState PreviousMouseState { get; private set; }
+        public MouseState CurrentMouseState { get; private set; }
+        public GamePadState PreviousControllerState { get; private set; }
+        public GamePadState CurrentControllerState { get; private set; }
+
+        public int DeltaScroll => CurrentMouseState.ScrollWheelValue - PreviousMouseState.ScrollWheelValue;
+        public int DeltaScrollHorizontal => CurrentMouseState.HorizontalScrollWheelValue - PreviousMouseState.HorizontalScrollWheelValue;
+
+        public Vector2 MousePosition => new Vector2(CurrentMouseState.X, CurrentMouseState.Y);
+
+        public bool UsingController => CurrentControllerState.IsConnected;
+
+        public GameInput()
+        {
+            _controls = new Dictionary<string, InputBinding>();
+        }
+
+        /// <summary>
+        /// Make sure this is called as early as possible in the Game's main Update method.
+        /// </summary>
+        public void UpdateInput()
+        {
+            PreviousKeyState = CurrentKeyState;
+            CurrentKeyState = Keyboard.GetState();
+
+            PreviousMouseState = CurrentMouseState;
+            CurrentMouseState = Mouse.GetState();
+
+            // Current controller implementation only checks for the first controller.
+            // Local multiplayer implementation required? Assuming not for now.
+            PreviousControllerState = CurrentControllerState;
+            CurrentControllerState = GamePad.GetState(PlayerIndex.One);
+        }
+
+        public void RegisterControl(string name, Keys defaultKeyboard, Buttons defaultController)
+        {
+            _controls[name] = new InputBinding(defaultKeyboard, defaultController);
+        }
+
+        public void RegisterControl(string name, MouseInput defaultMouse, Buttons defaultController)
+        {
+            _controls[name] = new InputBinding(defaultMouse, defaultController);
+        }
+
+        public InputBinding this[string name] => _controls[name];
+    }
+}
