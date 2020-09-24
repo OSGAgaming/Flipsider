a2c3cfeeb9b7cb0d1bd264a23b76fff3050ce649
OSGAgaming <41522151+OSGAgaming@users.noreply.github.com>
2020-09-24 11:39:09 +0100

    p

--- Flipsider/NPC.cs
+++ Flipsider/NPC.cs
@@ -14,12 +14,10 @@ namespace Flipsider
 
         public int life;
         public int maxLife;
-        protected void Jump(float jumpheight, bool hasToBeOnGround = true)
+        protected virtual void Jump(float jumpheight, bool hasToBeOnGround = true)
         {
             if (hasToBeOnGround ? onGround : true)
-            {
                 velocity.Y -= jumpheight;
-            }
         }
         public struct NPCInfo
         {
