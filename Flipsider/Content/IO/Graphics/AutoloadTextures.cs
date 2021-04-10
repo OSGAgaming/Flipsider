5d308fbce297b2082628bce8cb4d19ec1e937c1d
OSGAGaming <tafida.a123@gmail.com>
2021-04-10 02:26:43 +0100

    more stuiff

--- Flipsider/Content/IO/Graphics/AutoloadTextures.cs
+++ Flipsider/Content/IO/Graphics/AutoloadTextures.cs
@@ -14,49 +14,6 @@ namespace Flipsider
         internal readonly static List<string> AssetPaths = new List<string>();
 
         internal static Dictionary<string, Texture2D> Assets = new Dictionary<string, Texture2D>();
-
-        public static string TextureCachePath => Utils.MainDirectory;
-        public static void GenerateTextureCache()
-        {
-            FileStream stream = File.Open(TextureCachePath + @"\AutoloadTextureCache.cs", FileMode.Create);
-            using (StreamWriter sw = new StreamWriter(stream))
-            {
-                sw.WriteLine($@"using Microsoft.Xna.Framework;");
-                sw.WriteLine($@"using Microsoft.Xna.Framework.Content;");
-                sw.WriteLine($@"using Microsoft.Xna.Framework.Graphics;");
-                sw.WriteLine($@"using System.Collections.Generic;");
-                sw.WriteLine($@"using System.Diagnostics;");
-                sw.WriteLine($@"using System.IO;");
-
-                sw.WriteLine($@"namespace Flipsider");
-                sw.WriteLine(@"{");
-
-                sw.WriteLine(@"   public static class Textures");
-                sw.WriteLine(@"   {");
-
-                sw.WriteLine(@"#nullable disable");
-
-                foreach (string s in AssetPaths)
-                {
-                    string NewString = s.Replace(@"\","_");
-                    sw.WriteLine(@$"     public static Texture2D _{NewString};");
-                }
-
-                sw.WriteLine(@"     public static void LoadTextures()");
-                sw.WriteLine(@"     {");
-                foreach (string s in AssetPaths)
-                {
-                    string NewString = s.Replace(@"\", "_");
-                    sw.WriteLine($"       _{NewString} = AutoloadTextures.Assets[@\"{s}\"];");
-                }
-                sw.WriteLine(@"     }");
-
-                sw.WriteLine(@"   }");
-
-                sw.WriteLine(@"}");
-            }
-
-        }
         public static void AddAssetsFromDirectories(string DirectoryPath)
         {
             if (Path.GetFileName(DirectoryPath) == "bin") return;
